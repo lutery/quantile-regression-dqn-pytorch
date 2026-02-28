28553a79220b64af3a774f7401807643a6a5aa6b
chenghui <luctery@foxmail.com>
2026-02-28 18:48:44 +0800

    正在补齐注释

--- rl_utils.py
+++ rl_utils.py
@@ -10,7 +10,13 @@ class ReplayMemory:
         self.memory = []
 
     def push(self, state, action, next_state, reward, done):
-        transition = torch.Tensor([state]), torch.Tensor([action]), torch.Tensor([next_state]), torch.Tensor([reward]), torch.Tensor([done])
+        transition = (
+            torch.as_tensor(state, dtype=torch.float32).unsqueeze(0),
+            torch.tensor([action], dtype=torch.long),
+            torch.as_tensor(next_state, dtype=torch.float32).unsqueeze(0),
+            torch.tensor([reward], dtype=torch.float32),
+            torch.tensor([done], dtype=torch.float32),
+        )
         self.memory.append(transition)
         if len(self.memory) > self.capacity: del self.memory[0]
 
@@ -18,11 +24,11 @@ class ReplayMemory:
         sample = random.sample(self.memory, batch_size)
         batch_state, batch_action, batch_next_state, batch_reward, batch_done = zip(*sample)
         
-        batch_state = torch.cat(batch_state)
-        batch_action = torch.LongTensor(batch_action)
-        batch_reward = torch.cat(batch_reward)
-        batch_done = torch.cat(batch_done)
-        batch_next_state = torch.cat(batch_next_state)
+        batch_state = torch.cat(batch_state, dim=0)
+        batch_action = torch.cat(batch_action, dim=0)
+        batch_reward = torch.cat(batch_reward, dim=0)
+        batch_done = torch.cat(batch_done, dim=0)
+        batch_next_state = torch.cat(batch_next_state, dim=0)
         
         return batch_state, batch_action, batch_reward.unsqueeze(1), batch_next_state, batch_done.unsqueeze(1)
     
